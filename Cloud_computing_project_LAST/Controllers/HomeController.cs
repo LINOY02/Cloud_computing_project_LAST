dc3d7056311c6872d35edc6ce5c2d161e1e02ad9
LINOY <linoyyaday@gmail.com>
2023-08-29 19:07:00 +0300

    Add project files.

---
+++ Cloud_computing_project_LAST/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Cloud_computing_project_LAST.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Cloud_computing_project_LAST.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Cloud_computing_project_LAST/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cloud_computing_project_LAST.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
