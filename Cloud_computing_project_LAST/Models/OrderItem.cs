3e9e66990cdac1746c170c230ad0d253b473073a
Tamar Gefner <tgwork640@gmail.com>
2023-12-03 20:32:58 +0200

    add hebcal, weather and orderItem

---
+++ Cloud_computing_project_LAST/Models/OrderItem.cs
@@ -0,0 +1,13 @@
+﻿namespace Cloud_computing_project_LAST.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int orderId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+    }
+}
---
+++ Cloud_computing_project_LAST/Models/Weather.cs
@@ -0,0 +1,17 @@
+﻿namespace Cloud_computing_project_LAST.Models
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public int orderId { get; set; }
+        public double Temp { get; set; }
+        public double FeelsLike { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public int Pressure { get; set; }
+        public int Humidity { get; set; }
+        public string? Main { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
